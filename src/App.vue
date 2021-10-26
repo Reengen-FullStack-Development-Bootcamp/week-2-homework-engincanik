29c77510dff9749dacf4f7564c1f9362bd50b717
engincanik <engincanik@gmail.com>
2021-10-26 23:04:39 +0300

    remove unused items & create hotel card design with icons

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,69 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="#d3eaed" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="./assets/hotel.svg"
           transition="scale-transition"
           width="40"
         />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-btn href="#" text style="font-size: 32px">Hotels</v-btn>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+      <div>
+      <v-autocomplete
+        label="Country"
+        color="primary"
+        cache-items
+        :search-input.sync="search"
+        hide-details
+        hide-no-data
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      </v-autocomplete>
+      </div>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <v-row no-gutters>
+        <v-col cols="6" v-for="n in 12" :key="n">
+          <MainPage/>
+        </v-col>
+      </v-row>
+      <!-- <MainPage /> -->
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import MainPage from "./components/MainPage";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    MainPage
   },
 
   data: () => ({
-    //
-  }),
+    data: null
+  })
 };
 </script>
+
+<style >
+.v-select.v-text-field input {
+  color: #800000 !important;
+  font-size:16px;
+}
+.v-select.v-text-field label {
+  color: #800000 !important;
+}
+.v-select {
+  color: #800000;
+}
+</style> 
