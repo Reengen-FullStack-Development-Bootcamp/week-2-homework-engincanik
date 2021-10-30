a2300a4a444f2a18a3a351df817558fa45e8e6f3
engincanik <engincanik@gmail.com>
2021-10-30 15:32:12 +0300

    Add hotel pictures; finish hotel detail design

--- src/components/HotelCard.vue
+++ src/components/HotelCard.vue
@@ -1,101 +1,49 @@
 <template>
   <v-card
-    @click="goToDetails(id)"
+    @click="goToDetails(id, hotel)"
     id="card"
     max-width="500"
+    height="500"
     class="mx-auto my-12"
   >
     <v-img height="250" src="../assets/logo.svg"></v-img>
-    <v-card-title>Hotel Name</v-card-title>
+    <v-card-title> {{ hotel.name }} </v-card-title>
     <v-card-text>
       <v-row align="center" class="mx-0">
         <v-rating
-          :value="4.5"
+          :value="hotel.rating"
           color="amber"
           dense
           half-increments
           readonly
           size="14"
         ></v-rating>
-        <div class="grey--text ms-4">4.5(413)</div>
+        <div class="grey--text ms-4">{{hotel.rating}} ({{hotel.ratingCount}}) </div>
       </v-row>
-      <div class="my-4 text-subtitle-1">$ Price • Country</div>
+      <div class="my-4 text-subtitle-1">
+        $ {{ hotel.price }} • {{ hotel.location }}
+      </div>
       <div>
-        The Jianguo Hotel Qianmen is located near Tiantan Park, just a 10-minute
-        walk from the National Center for the Performing Arts and Tian'anmen
-        Square. Built in 1956 it has old school charm and many rooms still
-        feature high, crown-molded ceilings.
+        {{ hotel.description }}
       </div>
     </v-card-text>
-    <!-- <v-divider class="mx-4"></v-divider> -->
-    <!-- <v-card-text>
-        <v-row class="d-flex justify-space-between">
-          <div class="ms-4 text-subtitle-1 blue--text d-flex align-center">
-            Guest amount:
-          </div>
-          <div class="d-flex align-center">
-            <v-btn class="mx-2" fab dark x-small color="#333333">
-              <v-icon dark>
-                mdi-minus
-              </v-icon>
-            </v-btn>
-            <v-card-text class="black--text">5</v-card-text>
-            <v-btn class="mx-2" fab dark x-small color="#333333">
-              <v-icon dark>
-                mdi-plus
-              </v-icon>
-            </v-btn>
-          </div>
-        </v-row>
-        <v-dialog
-          ref="dialog"
-          v-model="modal"
-          :return-value.sync="date"
-          persistent
-          width="290px"
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field
-              v-model="dateRangeText"
-              label="Reservation Dates"
-              prepend-icon="mdi-calendar"
-              readonly
-              v-bind="attrs"
-              v-on="on"
-            ></v-text-field>
-          </template>
-          <v-date-picker
-            v-model="dates"
-            range
-            scrollable
-            :min="new Date().toISOString().substr(0, 10)"
-          >
-            <v-spacer></v-spacer>
-            <v-btn text color="primary" @click="modal = false">
-              Cancel
-            </v-btn>
-            <v-btn text color="primary" @click="$refs.dialog.save(date)">
-              OK
-            </v-btn>
-          </v-date-picker>
-        </v-dialog>
-      </v-card-text> -->
   </v-card>
 </template>
 
 <script>
 export default {
   props: {
-    id: Number
+    id: String,
+    hotel: Object
   },
   data() {
     return {
-      peopleCount: 0,
+      peopleCount: 0
     };
   },
   methods: {
-    goToDetails(id) {
-      this.$router.push({ name: "HotelDetail", params: { id: id } });
+    goToDetails(id, hotel) {
+      this.$router.push({ name: "HotelDetail", params: { id: id, selectedHotel: hotel} });
     }
   }
 };
--- src/views/HotelDetail.vue
+++ src/views/HotelDetail.vue
@@ -1,15 +1,15 @@
 <template>
   <v-row>
     <v-col class="align-center" cols="6">
-      <h1>Hotel Name</h1>
-      <h3 class="mt-2">Desc</h3>
+      <h1>{{ selected.name }}</h1>
+      <h3 class="mt-2">{{ selected.description }}</h3>
       <v-row class="mt-2">
         <v-col class="d-flex align-center">
           <div>
             <v-icon>
               mdi-map-marker
             </v-icon>
-            Hotel Location
+            {{ selected.location }}
           </div>
         </v-col>
         <v-col>
@@ -23,6 +23,7 @@
             <template v-slot:activator="{ on, attrs }">
               <v-text-field
                 v-model="dateRangeText"
+                hint="Check-in and Check-out dates"
                 label="Reservation Dates"
                 prepend-icon="mdi-calendar"
                 readonly
@@ -47,9 +48,54 @@
           </v-dialog>
         </v-col>
       </v-row>
+      <v-row class="justify-center">
+        <v-card outlined elevation="3" color="#F6F6F6">
+          <v-card-text class="blue--text"> $ {{ selected.price }} </v-card-text>
+        </v-card>
+      </v-row>
+      <v-divider class="my-8"></v-divider>
+      <div class="mt-4">
+        <v-row class="d-flex justify-space-between">
+          <div class="ms-4 text-subtitle-1 blue--text d-flex align-center">
+            Guest amount:
+          </div>
+          <div class="d-flex align-center">
+            <v-btn
+              @click="guestCount >= 1 ? guestCount-- : guestCount"
+              class="mx-2"
+              fab
+              dark
+              x-small
+              color="#333333"
+            >
+              <v-icon dark>
+                mdi-minus
+              </v-icon>
+            </v-btn>
+            <v-card-text class="black--text">{{ guestCount }}</v-card-text>
+            <v-btn
+              @click="guestCount++"
+              class="mx-2"
+              fab
+              dark
+              x-small
+              color="#333333"
+            >
+              <v-icon dark>
+                mdi-plus
+              </v-icon>
+            </v-btn>
+          </div>
+        </v-row>
+        <v-row class="mt-4 justify-center">
+          <v-btn @click="reserve()" outlined elevation="2" color="primary"
+            >Reserve</v-btn
+          >
+        </v-row>
+      </div>
     </v-col>
     <v-col cols="6" class="d-flex align-center">
-      <v-img height="500px" width="500px" contain src="@/assets/logo.svg" />
+      <v-img height="500px" width="500px" contain :src="getImage" />
     </v-col>
   </v-row>
 </template>
@@ -60,6 +106,8 @@ export default {
     return {
       dates: [],
       modal: false,
+      selected: Object,
+      guestCount: 0,
       date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
         .toISOString()
         .substr(0, 10)
@@ -68,7 +116,31 @@ export default {
   computed: {
     dateRangeText() {
       return this.dates.join(" - ");
+    },
+    getImage() {
+      return require("@/" + this.selected.image);
     }
+  },
+  methods: {
+    reserve() {
+      if (this.guestCount !== 0 && this.dates.length > 1) {
+        this.$router.push({
+          name: "Reservation",
+          params: { guestCount: this.guestCount }
+        });
+      } else {
+        this.$toasted.show("Please select reservation date & guest count", {
+          theme: "toasted-primary",
+          position: "bottom-left",
+          iconPack: "mdi",
+          icon: "alert",
+          duration: 2000
+        });
+      }
+    }
+  },
+  mounted: function() {
+    this.selected = this.$route.params.selectedHotel;
   }
 };
 </script>
